7f05d46a664b4569a09be72a2481ce79c3b4a271
Your Name <riftholder188@gmail.com>
2025-06-25 18:47:06 -0400

    Fully Implemented Pandas DataFrame

--- selphone/selectPhonemes.py
+++ selphone/selectPhonemes.py
@@ -1,17 +1,21 @@
 import random
 import sys
+import numpy as np
+import pandas as pd
+import time
 from collections import Counter
 
 sys.path.append(".")
 from probs.phoneProb import calcBaseProb
-from utils.phoneLinkedList import PhonemeList
 
-def adjustProb(phonemes: PhonemeList, sel_bin, laryngs, num_types):
-    curr_phone = phonemes.getHead()
+def adjustProb(phonemes: pd.DataFrame, sel_bin, laryngs, num_types):
     
-    for _ in range(phonemes.getCount()):
-        curr_bin = curr_phone.getBin()
-        curr_prob = curr_phone.getProb()
+    for phoneme in phonemes.index:
+        if phonemes.at[phoneme, "Selected"]:
+            continue
+
+        curr_bin = phonemes.at[phoneme, "Binary"]
+        curr_prob = phonemes.at[phoneme, "Probability"]
 
         # Place (make consonants in current place more likely, and makes consonants in nearby places less likely)
         sel_place = sel_bin % 32
@@ -65,9 +69,9 @@ def adjustProb(phonemes: PhonemeList, sel_bin, laryngs, num_types):
             else: # Last consonant picked was an sonorant
                 curr_prob -= 0.01
 
-        phonemes.modNodeProb(curr_bin, curr_prob)
-        curr_phone = curr_phone.getNext()
-
+        phonemes.at[phoneme, "Probability"] = curr_prob
+        
+    phonemes.sort_values(by="Probability", ascending=False)
     return
 
 
@@ -78,37 +82,33 @@ and then, based on temp, phonemes up to a certain lower probability are also
 considered. Next, one of these phonemes are randomly selected, and every 
 other phoneme's probability is adjusted based on the selection.
 """
-def selectPhonemes(phonemes: PhonemeList, num_phones, temp):
+def selectPhonemes(phonemes: pd.DataFrame, num_phones, temp):
     selected_phones = []
     laryngs = set()
     num_types = Counter()
+
     for _ in range(num_phones):
-        sel_phone_list = []
 
         # Choose phonemes to select from
-        head = phonemes.getHead()
-        min_prob = head.getProb() - (12 * temp)
+        unsel_phonemes = phonemes[phonemes["Selected"] == False]
+        min_prob = unsel_phonemes.head(1).at[unsel_phonemes.index[0], "Probability"] - (12 * temp)
 
-        sel_phone_list = [head]
-        curr_phone = head
+        sel_phone_frame = phonemes[(phonemes["Probability"] > min_prob) & (phonemes["Selected"] == False)]
 
-        while curr_phone.getProb() > min_prob:
-            sel_phone_list += [curr_phone]
-            curr_phone = curr_phone.getNext()
-
-            if not curr_phone:
-                break
-
-        # print(sel_phone_list)
+        # print(min_prob)
+        # print(phonemes)
+        # print(sel_phone_frame)
         # print()
 
         # Randomly select phoneme from sel_phone_list
-        pick = sel_phone_list[random.randint(0, len(sel_phone_list) - 1)]
-        selected_phones += [pick.getPhoneme()]
+        pick = sel_phone_frame.sample()
+        picked_phoneme = pick.index[0]
+        
+        selected_phones += [picked_phoneme]
+        phonemes.at[picked_phoneme, "Selected"] = True
 
         # Adjust probabilities based on selected phoneme
-        sel_bin = pick.getBin()
-        phonemes.removeNode(sel_bin)
+        sel_bin = pick.at[picked_phoneme, "Binary"]
 
         if (sel_bin >> 8) % 2 == 0:
             laryngs.add((sel_bin >> 5) % 8)
